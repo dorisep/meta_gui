81d16ef2f4b2cfc8e30d29c6848b68c3c2596a5d
dorisep <ed.doris@gmail.com>
2022-02-14 13:54:23 -0600

    New feature-visualizations.
    created some ranking visualizations for weekly scrape and scatterplots for correlation.

--- apps/genre_clean.py
+++ apps/genre_clean.py
@@ -7,9 +7,4 @@ with open('..//data//meta_scrape.csv') as file:
     genre_lst = []
     for row in reader:
         print(row)
-#         if len(row) > 0:
-#             print(row[10])
-#             # for genres in row[10]:
-#             #     if genres not in genre_lst:
-#             #         genre_lst.append(genres)
-# print(genre_lst)
+# clear
